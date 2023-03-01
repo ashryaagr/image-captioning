2ed31cb35e70ac62241f15fc015461359787b8a9
AGhafaryy <ang08@mail.aub.edu>
2023-02-28 20:06:29 -0800

    Added ResnetLSTM, train, val, test
    Co-authored-by: Jay Jhaveri <jjhaveri1906@gmail.com>

--- file_utils.py
+++ file_utils.py
@@ -6,9 +6,16 @@ def read_file(path):
     if os.path.isfile(path):
         with open(path) as json_file:
             data = json.load(json_file)
-        return data
+        
     else:
+#         with open(path, 'w') as f: # Doing this because TA code wasn't creating an empty [] in training_losses.txt and val_losses.txt
+#             f.write(str([]))  
+        
+#         with open(path) as json_file:
+#             data = json.load(json_file)
+#     return data
         raise Exception("file doesn't exist: ", path)
+    return data
 
 
 def read_file_in_dir(root_dir, file_name):
